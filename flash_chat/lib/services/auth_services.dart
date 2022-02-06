9b185707f6131dd0548b8cef0be3d07e62d76510
MarceloJSCesar <jsdevmarcelo@gmail.com>
2022-02-06 09:00:24 -0800

    user class named to person

--- flash_chat/lib/services/auth_services.dart
+++ flash_chat/lib/services/auth_services.dart
@@ -3,6 +3,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
   final _instance = FirebaseFirestore.instance;
-  Future register() async {}
+  Future<User> register() async {}
   Future login() async {}
 }
