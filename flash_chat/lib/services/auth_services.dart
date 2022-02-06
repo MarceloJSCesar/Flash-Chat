d11647496d59a1b94f9f0556d677e4999000077a
MarceloJSCesar <jsdevmarcelo@gmail.com>
2022-02-06 08:55:01 -0800

    auth services created

---
+++ flash_chat/lib/services/auth_services.dart
@@ -0,0 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final _instance = FirebaseFirestore.instance;
+  Future register() async {}
+  Future login() async {}
+}
